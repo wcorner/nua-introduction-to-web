228c5648703b3e27c2803d24073a9e403f4eccb5
William Corner <w.corner@norwichuni.ac.uk>
2025-10-20 15:33:37 +0100

    Update example with playground.js

---
+++ example/assets/playground.js
@@ -0,0 +1,7 @@
+const cATechs = ["Hannah", "Hannah-Louise", "Will", "Tanje", "Linards", "Tracey", "Matthew"];
+for (const tech of cATechs) { 
+  if (tech === "Tanje") {
+  continue; 
+}
+console.log(tech); 
+}
