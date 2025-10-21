5790cf2e7387132457199d5d06c94e36b77671d1
William Corner <w.corner@norwichuni.ac.uk>
2025-10-21 12:50:19 +0100

    Add workshop changes

--- 03 Introduction to JavaScript/temperature-conversion/temp-conversion.js
+++ 03 Introduction to JavaScript/temperature-conversion/temp-conversion.js
@@ -1,3 +1,31 @@
+document.getElementById("temp").value = null;
+
+let button = document.getElementById("button");
+
 const tempCalc = function (){
-    
+
+    let kelvin = document.getElementById("temp").value;   
+    kelvin = Number(kelvin);
+
+    const celsius = Math.floor(kelvin - 273);
+    const fahrenheit = Math.floor(celsius * (9 / 5) + 32);
+    const rankine = Math.floor(kelvin * 1.8);
+
+    const kValue = document.getElementById("kelvin");
+    const cValue = document.getElementById("celsius");
+    const fValue = document.getElementById("fahrenheit");
+    const rValue = document.getElementById("rankine");
+
+    kValue.innerHTML = `The temperature is ${kelvin.toString()} degrees Kelvin.`;
+    cValue.innerHTML = `The temperature is ${celsius.toString()} degrees Celsius.`;
+    fValue.innerHTML = `The temperature is ${fahrenheit.toString()} degrees Fahrenheit.`;
+    rValue.innerHTML = `The temperature is ${rankine.toString()} degrees Rankine.`; 
 };
+
+button.addEventListener("click", tempCalc)
+
+document.addEventListener("keydown", (event) => {
+    if(event.key === " ") {
+        tempCalc();
+    }
+})
